42f17ed5fda1049547af9f57901632232d1681c1
markkukynsijarvi <markku.kynsijarvi@iki.fi>
2026-02-19 21:10:39 +0200

    simple unsynced bullet system

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,13 +3,17 @@ extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
 @onready var weapon_root: Node2D = $WeaponRoot
+
+var bullet_scene: PackedScene = preload("uid://cmsm71jq22qef")
+
 var input_multiplayer_authority : int
-
-
-
 
 func _ready() -> void:
 	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack"):
+		create_bullet()
 
 func _process(_delta: float) -> void:
 	var aim_position = weapon_root.global_position + player_input_synchronizer_component.aim_vector
@@ -18,3 +22,10 @@ func _process(_delta: float) -> void:
 	if is_multiplayer_authority():
 		velocity = player_input_synchronizer_component.movement_vector * 100
 		move_and_slide()
+
+func create_bullet():
+	var bullet = bullet_scene.instantiate() as Bullet
+	bullet.global_position = weapon_root.global_position
+	
+	get_parent().add_child(bullet)
+	bullet.start(player_input_synchronizer_component.aim_vector)
