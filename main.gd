d0db3e479c9d7942a4be80a0da6e06f97a9a2604
markkukynsijarvi <markku.kynsijarvi@iki.fi>
2026-02-24 10:33:51 +0200

    added simple arena with area blocks and spawning player in arena

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 var player_scene: PackedScene = preload("uid://egtpvj3ddlhx")
 var enemy_scene: PackedScene = preload("uid://dea55va7q7xfs")
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
+@onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,12 +11,13 @@ func _ready() -> void:
 		var player = player_scene.instantiate() as Player
 		player.name = str(data.peer_id)
 		player.input_multiplayer_authority = data.peer_id
+		player.global_position = player_spawn_position.global_position
 		return player
 	peer_ready.rpc_id(1)
 	
 	if is_multiplayer_authority():
 		var enemy = enemy_scene.instantiate() as Node2D
-		enemy.global_position = Vector2.ONE * 100
+		enemy.global_position = Vector2.ONE * 200
 		add_child(enemy)
 
 
