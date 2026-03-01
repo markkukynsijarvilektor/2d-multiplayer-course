f677c23303543b1883aac949ec0c521127cd31f2
markkukynsijarvi <markku.kynsijarvi@iki.fi>
2026-03-01 16:28:59 +0200

    enemy "ai"

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -1,16 +1,45 @@
 extends CharacterBody2D
 
 @onready var area_2d: Area2D = $Area2D
+@onready var target_acquisition_timer: Timer = $TargetAcquisitionTimer
+
 
 var current_health: int = 5
+var target_position : Vector2
 
 func _ready():
 	area_2d.area_entered.connect(_on_area_entered)
+	target_acquisition_timer.timeout.connect(_on_target_acquisition_timer_timeout)
+	if is_multiplayer_authority():
+		acquire_target()
+	
+func _process(delta: float) -> void:
+	if is_multiplayer_authority():
+		velocity = global_position.direction_to(target_position) * 40
+		move_and_slide()
 	
 func handle_hit():
 	current_health -= 1
 	if current_health <= 0:
 		queue_free()	
+
+func acquire_target():
+	var players = get_tree().get_nodes_in_group("player")
+	var nearest_player: Player = null
+	var nearest_squared_distance : float
+	
+	for player in players:
+		if nearest_player == null:
+			nearest_player = player
+			nearest_squared_distance = nearest_player.global_position.distance_squared_to(global_position)
+			continue
+		var player_squared_distance: float = player.global_position.distance_squared_to(global_position) 
+		if player_squared_distance < nearest_squared_distance:
+			nearest_player = player
+			nearest_squared_distance = player_squared_distance
+	
+	if nearest_player != null:
+		target_position = nearest_player.global_position
 
 func _on_area_entered(other_area: Area2D):
 	# not server?
@@ -22,3 +51,7 @@ func _on_area_entered(other_area: Area2D):
 		bullet.register_collision()
 		handle_hit()
 		
+
+func _on_target_acquisition_timer_timeout():
+	if is_multiplayer_authority():
+		acquire_target()
